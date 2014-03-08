36afcff5ae7fd31bc1694ecdd0689ef1236384c6
Jelmer Vernooij <jelmer@debian.org>
2014-03-08 23:39:54 +0000

    Add server/base_prefix setting.

--- calypso/config.py
+++ calypso/config.py
@@ -46,6 +46,7 @@ INITIAL_CONFIG = {
         "certificate": "/etc/apache2/ssl/server.crt",
         "key": "/etc/apache2/ssl/server.key",
         "pidfile": "/var/run/calypso.pid",
+        "base_prefix": "/",
     },
     "encoding": {
         "request": "utf-8",
